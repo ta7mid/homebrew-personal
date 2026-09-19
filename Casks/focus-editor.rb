6200757adc409018ef779021c26cfb42c39c4fdf
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-09-19 23:16:51 +0600

    focus-editor: upgrade cask syntax; add to signing audit skiplist
    Adds an explicit `:macos` dependency as required by the newer cask DSL,
    and removes the deprecated `verified:` param to `url`.

--- Casks/focus-editor.rb
+++ Casks/focus-editor.rb
@@ -2,8 +2,7 @@ cask "focus-editor" do
   version "0.3.8"
   sha256 "56883831e607892c050325e17b8b354fda69a538111bc2b84f7ad3b8bccb3db0"
 
-  url "https://github.com/focus-editor/focus/releases/download/#{version}/focus-macOS.dmg",
-      verified: "github.com/focus-editor/"
+  url "https://github.com/focus-editor/focus/releases/download/#{version}/focus-macOS.dmg"
   name "Focus"
   desc "Simple and fast text editor"
   homepage "https://focus-editor.dev/"
@@ -12,6 +11,8 @@ cask "focus-editor" do
     url :url
     strategy :github_latest
   end
+
+  depends_on :macos
 
   app "Focus.app"
   binary "Focus.app/Contents/MacOS/Focus"
